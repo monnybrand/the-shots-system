f07023477fe9c1422f592edd765f97618bb27b03
monnybrand <francishaymon83@gmail.com>
2026-02-09 16:39:40 -0800

    Update db.js

--- db.js
+++ db.js
@@ -4,8 +4,10 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port:process.env.DB_PORT
 });
 
 module.exports = connection;
+
 
