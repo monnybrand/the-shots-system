c2d85ce65df16c639cdfb44e413321bdc44477e9
monnybrand <francishaymon83@gmail.com>
2026-02-09 16:08:30 -0800

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",          // your MySQL password
-    database: "videography_sme"
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-db.connect(err => {
-    if (err) console.error("DB connection error:", err);
-    else console.log("✅ MySQL Connected");
-});
+module.exports = connection;
 
-module.exports = db;
