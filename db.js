11748e6202cfff62b64594a4e368311625fbb249
monnybrand <francishaymon83@gmail.com>
2026-02-11 15:50:32 -0800

    Update db.js

--- db.js
+++ db.js
@@ -5,10 +5,22 @@ const connection = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port:process.env.MYSQLPORT
+  port:process.env.MYSQLPORT,
+  ssl:{
+    rejectUnauthorized: false
+  }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL connection error:", err);
+  } else {
+    console.log("MySQL connected successfully (Railway)");
+  }
 });
 
 module.exports = db;
+
 
 
 
