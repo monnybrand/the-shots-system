09b76cc8aba3ef5e79c2f788079cc946750ff5cd
monnybrand <francishaymon83@gmail.com>
2026-02-11 16:20:31 -0800

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,22 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port:process.env.MYSQLPORT,
-  ssl:{
+  port: process.env.MYSQLPORT,
+  ssl: {
     rejectUnauthorized: false
   }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("MySQL connection error:", err);
+    console.error("❌ MySQL connection error:", err);
   } else {
-    console.log("MySQL connected successfully (Railway)");
+    console.log("✅ MySQL connected successfully (Railway)");
   }
 });
 
 module.exports = db;
-
-
-
-
